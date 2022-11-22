5a0bbd91b319a7766daca2c692685fef14596ed4
Lirodek <96456897+Lirodek@users.noreply.github.com>
2022-11-22 19:35:25 +0900

    testCommit
    aa

--- ArrayStudy/0109PhoneBook/src/PhoneBook07Egtukeul/PhoneEgrukeulMain.java
+++ ArrayStudy/0109PhoneBook/src/PhoneBook07Egtukeul/PhoneEgrukeulMain.java
@@ -8,7 +8,7 @@ public class PhoneEgrukeulMain {
 		PhoneBookManager manager = PhoneBookManager.createManagerInst();
 		JFrame winFrame = new SearchDelFrame("PhoneBookVer09");
 		int choice;
-		
+		int hihi;
 		while(true) {
 			try {
 				MenuViewer.showMenu();
