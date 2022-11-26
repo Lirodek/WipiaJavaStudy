a89b7bade39835dd2f3ed35ba28395217613c6a4
Lirodek <96456897+Lirodek@users.noreply.github.com>
2022-11-26 18:28:35 +0900

    2022-11-26 java object class
    most done last remaining finalize

--- javauseFullPackage/Object/src/wipia/study/java/javaObj.java
+++ javauseFullPackage/Object/src/wipia/study/java/javaObj.java
@@ -2,84 +2,78 @@ package wipia.study.java;
 
 import java.util.Objects;
 
+import wipia.study.property.Person;
+
 public class javaObj {
-	public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) throws CloneNotSupportedException {
 		//getClassEx(new Minwoo());
-		hashCodeEx();
+		//hashCodeEx();
+		//equalsEx();
+		//cloneEx();
+		finalizeEx();
+		
+		
+		
 	}
 	
+	// Object를 입력받아 해당 객체가 어느 클래스인지 비교합니다.
 	private static void getClassEx(Object obj) {
-		GwangMin lirodek = new GwangMin();
+		Person lirodek = new Person();
 		System.out.println(lirodek.getClass());
 		System.out.println(obj.getClass());
 	}
 	
+	// 해쉬코드 예제
 	private static void hashCodeEx() {
 //		String hello = "안녕하세요	";
 //		String hellokr = "안녕하세요";
 //		System.out.println(hello.hashCode());
 //		System.out.println(hellokr.hashCode());
 		
-		System.out.println(new Person("배광민").hashCode());
-		System.out.println(new Person("배광민").hashCode());
+		System.out.println(new Person("배광민", 25).hashCode());
+		System.out.println(new Person("김선경", 25).hashCode());
 	}
-
-
-}
-class Person{
-	private long id;
-	private String name;
-	@Override
-	public int hashCode() {
-		int hash = 7;
-	    hash = 31 * hash + name.hashCode();
-	    hash = 31 * hash + (name == null ? 0 : name.hashCode());
-	    return hash;
+	
+	// 이퀄스 예제
+	private static void equalsEx() {
+		String str1 = "abc";
+		String str2 = str1;
+		String str3 = new String("defabc");
+		str3 = str3.substring(3);
+		System.out.println(str3);
+		System.out.println("=====  ( == )예제 =====");
+		
+		System.out.println("String str2 = str1; 	       :: " + (str1 == str2));
+		System.out.println("String str3 = new String(abc); :: " + (str1 == str3));
+		
+		System.out.println("===== equals 예제 =====");
+		
+		System.out.println("String str2 = str1; 	       :: " + str1.equals(str2));
+		System.out.println("String str3 = new String(abc); :: " + str1.equals(str3));
 		
 	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		return Objects.equals(name, other.name);
-	}
-
-	@Override
-	public String toString() {
-		return "Person [name=" + name + "]";
-	}
-
-	public Person(String name) {
-		super();
-		this.name = name;
-	}
-
-	public Person() {
-		super();
-		// TODO Auto-generated constructor stub
+	// 클론 예제
+	private static void cloneEx() throws CloneNotSupportedException {
+		Person p1 = new Person("배광민", 25);
+		Person p2 = new Person("김선경", 25);
+		p1.setParents(new Person("곽혜연", 47));
+		Person p3 = (Person)p1.clone();
+		p1.setParents(new Person("혜연", 47));
+		System.out.println(p3.getParents().toString());
 	}
 	
-}
-class Minwoo{
-	
+	// 파이널라이즈 예제
+	private static void finalizeEx() {
+		for(int i = 0; i < 20; i++) {
+			Person test = new Person("배광민"+i, i);
+			
+			test = null;
+			//null 값을 넣어 기존에 있는 값을 쓰레기로 만듬
+			System.gc();
+		}
+	}
 }
 
-class GwangMin{
-	
-}
