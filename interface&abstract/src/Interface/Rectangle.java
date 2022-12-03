dfed4e1e752edf93a1dd7c49e570afe42be43774
mwj5012 <mwj5012@naver.com>
2022-12-03 21:19:07 +0900

    추상클래스 인터페이스

---
+++ interface&abstract/src/Interface/Rectangle.java
@@ -0,0 +1,27 @@
+package Interface;
+//package Study221201;
+//
+//abstract class Shape {
+//	public String name;
+//	public String getName() {	// 일반 메서드
+//		return name;
+//	}
+//	
+//	// 추상메서드
+//	public abstract int getArea();		// 넓이
+//	public abstract int getCircum();	// 둘레
+//}
+//
+//public class Rectangle extends Shape {
+//	private int length;
+//	private int width;
+//	
+//	public Rectangle(String name, int length, int width) {
+//		this.name = name;
+//		this.length = length;
+//		this.width = width;
+//	}
+//	
+//	
+//	
+//}
